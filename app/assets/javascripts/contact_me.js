eae32fb10a71c96b47e8dd6827913a5bb6ddc3d6
David <davidmeza@outlook.com>
2015-07-27 00:50:52 -0400

    change layout

--- app/assets/javascripts/contact_me.js
+++ app/assets/javascripts/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "/assets/contact_me.php",
+                url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
