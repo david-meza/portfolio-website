932b045c7ec84ec5a3411c8f36cabaa67475c36b
David <davidmeza@outlook.com>
2015-08-07 01:14:40 -0400

    add to danebook to portfolio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,21 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Ignore bad email addresses and do not raise email delivery errors.
+  config.action_mailer.default_url_options = { host: 'davidmeza.herokuapp.com', port: ENV['PORT'] || 3000 }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp-mail.outlook.com",
+  port: 587,
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["EMAIL_USERNAME"],
+  password: ENV["EMAIL_PASSWORD"]
+  }
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
